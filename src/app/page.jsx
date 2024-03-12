4a45b0197476bd11fbc760caacb8c532e8d9b502
Eddy Léon <leoneddy504@gmail.com>
2024-03-12 16:46:04 +0100

    Fix footer bug 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ const Home = async ({searchParams}) => {
   return (
     <div>
       <Filter page={page} />
-      <div className='container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-6 min-h-[300px] pt-6 pb-6'>
+      <div className='container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-6 pt-6 pb-6'>
         { posts?.length === 0 ? 
             <div className="w-full h-full col-span-3 flex justify-center items-center">
               <div className="text-center text-base sm:text-xl md:text-2xl text-gray-400 font-semibold">
